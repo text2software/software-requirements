9f49540632820b412c77c377332253c95f6a89ab
Welvis <welvisconta@gmail.com>
2022-09-27 22:40:19 -0300

    update

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,8 +2,11 @@ const update = () => {
     let select = document.getElementById("people_type");
     let option = select.options[select.selectedIndex].text;
     
-    option == "Pessoa Jurídica" ? 
-        document.getElementById("teste").style.visibility = "hidden"
-        :
-        document.getElementById("teste").style.visibility = "visible"
+    if (option == "Pessoa Jurídica") {
+        document.getElementById("physical_person").style.visibility = "hidden"
+        document.getElementById("legal_person").style.visibility = "visible"
+    } else {
+        document.getElementById("physical_person").style.visibility = "visible"
+        document.getElementById("legal_person").style.visibility = "hidden"
+    }
 }
